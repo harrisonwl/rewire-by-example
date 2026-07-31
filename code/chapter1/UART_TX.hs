641d8b57c6e40d812bf807dcceb09d8bcfc6ea17
harrisonwl <harrisonwl@users.noreply.github.com>
2026-07-31 18:45:24 +0000

    deploy: 14b2c8689eef832379cd1cf462d93120fe8958b5

--- code/chapter1/UART_TX.hs
+++ code/chapter1/UART_TX.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE DataKinds #-}
 
-import ReWire
+import ReWire hiding (lift, signal , put , get , modify)
 import ReWire.Bits
 import ReWire.Finite
 import ReWire.Vectors
+
+import ReWire.Interactive (xshow,pp,bshow)
+import ExtensionalSemantics
 
     -- uart_tx inputs
     -- input clk,
@@ -15,6 +18,7 @@ import ReWire.Vectors
 -- | [9] in Cryptol
 data I = TX_DATA (W 8)
        | Nop
+
 -- TX_START
 
     -- output reg tx_pin,
@@ -38,28 +42,34 @@ tx_done (_ , td , _) = td
 data_reg :: S -> W 8
 data_reg (_ , _ , dr) = dr
 
-clrTX_PIN , clrTX_Done , setTX_PIN :: StateT S Identity ()
+-- clrTX_PIN , clrTX_Done , setTX_PIN :: StateT S Identity ()
+clrTX_PIN , clrTX_Done , setTX_PIN :: ST S ()
 setTX_PIN  = modify (\ (_ , td , dr) -> (True , td , dr))
 clrTX_PIN  = modify (\ (_ , td , dr) -> (False , td , dr)) 
 clrTX_Done = modify (\ (tp , _ , dr) -> (tp , False , dr))
 
-setDR :: W 8 -> StateT S Identity ()
+-- setDR :: W 8 -> StateT S Identity ()
+setDR :: W 8 -> ST S ()
 setDR w    = modify (\ (tp , td , _) -> (tp , td , w))
 
-getDR :: StateT S Identity (W 8)
+-- getDR :: StateT S Identity (W 8)
+getDR :: ST S (W 8)
 getDR = get >>= return . data_reg 
 
-genout :: ReacT I O (StateT S Identity) I
+-- genout :: ReacT I O (StateT S Identity) I
+genout :: Re I S O I
 genout = do
            s <- lift get
            signal (tx_pin s , tx_done s)
 
-out_pin :: Finite 8 -> StateT S Identity ()
+-- out_pin :: Finite 8 -> StateT S Identity ()
+out_pin :: Finite 8 -> ST S ()
 out_pin i = do
               (_ , td , dr) <- get
               put (dr `index` i , td , dr)
 
-idle , data_bits :: I -> ReacT I O (StateT S Identity) ()
+-- idle , data_bits :: I -> ReacT I O (StateT S Identity) ()
+idle , data_bits :: I -> Re I S O ()
 idle Nop          = do
                       lift setTX_PIN
                       lift clrTX_Done
@@ -93,11 +103,33 @@ data_bits _   = do
                   genout
                   lift (out_pin (finite 7))
                   i <- genout
+                  lift (setDR (lit 0) >> clrTX_PIN >> clrTX_Done)
                   idle i
 
 s0 :: S
 s0 = (True , False , lit 0)
 
-start :: ReacT I O Identity ()
-start = extrude (idle Nop) s0
+-- start :: ReacT I O Identity ()
+-- start :: Re I O Identity ()
+-- start = extrude (idle Nop) s0
                   
+start = re_inf (idle Nop) (Nop , s0 , (False , False))
+
+is0 :: Stream I
+is0 = TX_DATA (lit 0xFF) :< rep Nop
+
+instance Show I where
+  show (TX_DATA w8) = "TX_DATA " Prelude.++ xshow w8
+  show Nop          = "Nop"
+
+urp (b,b',w) = "(" Prelude.++ ppbit b Prelude.++ "," Prelude.++
+                              ppbit b' Prelude.++ "," Prelude.++
+                              bshow w Prelude.++ ")"
+   where
+     ppbit True  = "1"
+     ppbit False = "0"
+
+grunt []                  = "\n"
+grunt ((i , s , o) : sns) = "(" Prelude.++ show i Prelude.++ "," Prelude.++ urp s
+                                Prelude.++ "," Prelude.++ pp o Prelude.++ ")"
+                                Prelude.++ "\n" Prelude.++ grunt sns
